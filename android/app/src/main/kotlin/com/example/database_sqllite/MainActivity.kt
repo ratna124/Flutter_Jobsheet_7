0ca864aa7c93f482a09cac6ff567e701035b939c
= <=>
2021-03-24 17:05:54 +0700

    Database sqflite

---
+++ android/app/src/main/kotlin/com/example/database_sqllite/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.database_sqllite
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Tambahkan Item',
+      theme: ThemeData(
+       
+        primarySwatch: Colors.blueGrey,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
